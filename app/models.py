b4e73f1209b6cdad71b37b657d2db0e35b8ef3c0
Henrique Vieira <henriquevieiraperes@gmail.com>
2026-07-16 13:04:43 -0300

    refactor (models): melhora as funções do ramalhete diferenciando metodos de classe e instancia.

--- app/models.py
+++ app/models.py
@@ -13,35 +13,24 @@ class Ramalhete(models.Model):
 
     def __str__(self):
         return f"Ramalhete de {self.usuario.username} em {self.data.strftime('%d/%m/%Y %H:%M:%S')}"
-    
-    def excluir_ramalhete_por_id(self, ramalhete_id):
-        try:
-            ramalhete = Ramalhete.objects.get(id=ramalhete_id)
-            ramalhete.delete()
-            return True
-        except Ramalhete.DoesNotExist:
-            return False
         
-    def editar_ramalhete_por_id(self, ramalhete_id, missa_comunhao=None, visita_ao_santissimo=None, exame_de_consciencia=None, leitura_espiritual_meditacao=None, sacrificio=None):
-        try:
-            ramalhete = Ramalhete.objects.get(id=ramalhete_id)
-            if missa_comunhao is not None:
-                ramalhete.missa_comunhao = missa_comunhao
-            if visita_ao_santissimo is not None:
-                ramalhete.visita_ao_santissimo = visita_ao_santissimo
-            if exame_de_consciencia is not None:
-                ramalhete.exame_de_consciencia = exame_de_consciencia
-            if leitura_espiritual_meditacao is not None:
-                ramalhete.leitura_espiritual_meditacao = leitura_espiritual_meditacao
-            if sacrificio is not None:
-                ramalhete.sacrificio = sacrificio
-            ramalhete.save()
-            return True
-        except Ramalhete.DoesNotExist:
-            return False
-        
-    def buscar_ramalhete_por_usuario(self, usuario):
-        return Ramalhete.objects.filter(usuario=usuario).order_by('-data')
+    def editar_ramalhete(self, missa_comunhao=None, visita_ao_santissimo=None, exame_de_consciencia=None, leitura_espiritual_meditacao=None, sacrificio=None):
+        if missa_comunhao is not None:
+            self.missa_comunhao = missa_comunhao
+        if visita_ao_santissimo is not None:
+            self.visita_ao_santissimo = visita_ao_santissimo
+        if exame_de_consciencia is not None:
+            self.exame_de_consciencia = exame_de_consciencia
+        if leitura_espiritual_meditacao is not None:
+            self.leitura_espiritual_meditacao = leitura_espiritual_meditacao
+        if sacrificio is not None:
+            self.sacrificio = sacrificio
+        self.save()
     
-    def get_total_ramalhetes_por_mes_ano(self, usuario, mes, ano):
-        return Ramalhete.objects.filter(usuario=usuario, data__month=mes, data__year=ano).count()
+    @classmethod
+    def buscar_ramalhete_por_usuario(cls, usuario):
+        return cls.objects.filter(usuario=usuario).order_by('-data')
+    
+    @classmethod
+    def get_total_ramalhetes_por_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).count()
