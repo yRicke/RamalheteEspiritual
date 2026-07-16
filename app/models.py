93f9e8bbedaebc64a1dd328b6c0069948727d5db
Henrique Vieira <henriquevieiraperes@gmail.com>
2026-07-16 13:12:28 -0300

    feat (models): adciona funções de buscas avançadas nos Ramalhetes.

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class Ramalhete(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     missa_comunhao = models.IntegerField()
     visita_ao_santissimo = models.IntegerField()
+    tercos = models.IntegerField()
     exame_de_consciencia = models.IntegerField()
     leitura_espiritual_meditacao = models.IntegerField()
     sacrificio = models.IntegerField()
@@ -28,9 +29,41 @@ class Ramalhete(models.Model):
         self.save()
     
     @classmethod
-    def buscar_ramalhete_por_usuario(cls, usuario):
+    def get_ramalhete_por_usuario(cls, usuario):
         return cls.objects.filter(usuario=usuario).order_by('-data')
     
     @classmethod
-    def get_total_ramalhetes_por_mes_ano(cls, usuario, mes, ano):
-        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).count()
+    def get_total_missas_comunhao_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).aggregate(total=models.Sum('missa_comunhao'))['total'] or 0
+    
+    @classmethod
+    def get_total_visitas_ao_santissimo_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).aggregate(total=models.Sum('visita_ao_santissimo'))['total'] or 0
+    
+    @classmethod
+    def get_total_tercos_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).aggregate(total=models.Sum('tercos'))['total'] or 0
+    
+    @classmethod
+    def get_total_exames_de_consciencia_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).aggregate(total=models.Sum('exame_de_consciencia'))['total'] or 0
+    
+    @classmethod
+    def get_total_leituras_espirituais_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).aggregate(total=models.Sum('leitura_espiritual_meditacao'))['total'] or 0
+    
+    @classmethod
+    def get_total_sacrificios_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return cls.objects.filter(usuario=usuario, data__month=mes, data__year=ano).aggregate(total=models.Sum('sacrificio'))['total'] or 0
+    
+    @classmethod
+    def get_total_geral_por_usuario_mes_ano(cls, usuario, mes, ano):
+        return {"missa_comunhao": cls.get_total_missas_comunhao_por_usuario_mes_ano(usuario, mes, ano),
+                "visita_ao_santissimo": cls.get_total_visitas_ao_santissimo_por_usuario_mes_ano(usuario, mes, ano),
+                "tercos": cls.get_total_tercos_por_usuario_mes_ano(usuario, mes, ano),
+                "exame_de_consciencia": cls.get_total_exames_de_consciencia_por_usuario_mes_ano(usuario, mes, ano),
+                "leitura_espiritual_meditacao": cls.get_total_leituras_espirituais_por_usuario_mes_ano(usuario, mes, ano),
+                "sacrificio": cls.get_total_sacrificios_por_usuario_mes_ano(usuario, mes, ano)
+                }
+
+    
